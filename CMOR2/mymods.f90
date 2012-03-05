28d2c1697641c61e397cd7da5051429374d67c2c
Gary Strand <strandwg@users.noreply.github.com>
2012-03-05 16:36:49 +0000

    Changes to templates to add modeling1 in ACD

--- CMOR2/mymods.f90
+++ CMOR2/mymods.f90
@@ -3,11 +3,11 @@
 !
 module max_parms
   !
-  integer,parameter::max_entries   = 10000
-  integer,parameter::max_exprmnt   = 10000
-  integer,parameter::max_cesm_vars =    10
-  integer,parameter::max_ncfiles   =  1000
-  integer,parameter::max_nchunks   =  1000
+  integer,parameter::max_entries   = 100000
+  integer,parameter::max_exprmnt   =  10000
+  integer,parameter::max_cesm_vars =     10
+  integer,parameter::max_ncfiles   =   1000
+  integer,parameter::max_nchunks   =   1000
   !
 end module max_parms
 !
