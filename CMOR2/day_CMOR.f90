86d79df9de1f62a63261b1cdd2663aef1cf3f357
Gary Strand <strandwg@users.noreply.github.com>
2012-06-02 16:56:42 +0000

    day_CMOR tweaks

--- CMOR2/day_CMOR.f90
+++ CMOR2/day_CMOR.f90
@@ -980,14 +980,16 @@ program day_CMOR
                     nchunks(ifile)= 5
                     tidx1(1:nchunks(ifile)) = (/    1, 12776, 25551, 38326, 51101/)      ! 1850, 1885, 1920, 1955, 1990
                     tidx2(1:nchunks(ifile)) = (/12775, 25550, 38325, 51100, 56940/)      ! 1884, 1919, 1954, 1989, 2005
-                 case ( 35040 )         ! RCP from 2005-2100, use only 2006 onwards, 19 * 5 year chunks
+                 case ( 35040 )         ! RCP from 2005-2100, use only 2006 onwards, 4 * 1 + 17 * 5 + 1 * 6 year chunks
                     nchunks(ifile)= 19
                     tidx1(1) =   366
-                    tidx2(1) =  2191
-                    do ic = 2,nchunks(ifile)
+                    tidx2(1) =  1825
+                    do ic = 2,nchunks(ifile)-1
                        tidx1(ic) = tidx2(ic-1) + 1
                        tidx2(ic) = tidx1(ic) + 1825
                     enddo
+                    tidx1(nchunks(ifile)) = tidx2(nchunks(ifile)-1) + 1
+                    tidx2(nchunks(ifile)) = ntimes(ifile,1)
                  case ( 34675 )         ! RCP from 2006-2100, use all times, 2 * 35y + 1 * 25y chunks
                     nchunks(ifile)= 3
                     tidx1(1:nchunks(ifile)) = (/    1, 12776, 25551/)      ! 2006, 2041, 2076
@@ -1001,6 +1003,7 @@ program day_CMOR
                     tidx1(1:nchunks(ifile)) = 1
                     tidx2(1:nchunks(ifile)) = ntimes(ifile,1)
                  end select
+                 write(*,'(''# chunks '',i3,'':'',20((i5,''-'',i5),'',''))') nchunks(ifile),(tidx1(ic),tidx2(ic),ic=1,nchunks(ifile))
                  do ic = 1,nchunks(ifile)
                     do it = tidx1(ic),tidx2(ic)
                        time_counter = it
