7bb4b33b666bad514748d60c449c5d6ae53e3b1a
Gary Strand <strandwg@users.noreply.github.com>
2012-06-14 03:51:51 +0000

    Omon_CMOR

--- CMOR2/Omon_CMOR.f90
+++ CMOR2/Omon_CMOR.f90
@@ -1022,6 +1022,7 @@ program Omon_CMOR
                     call read_var(myncid(ifile,1),var_info(var_found(ifile,1))%name,indat2a)
                     !
                     tval(1) = time(it) ; tbnd(1,1) = time_bnds(1,it) ; tbnd(2,1) = time_bnds(2,it)
+                    write(*,'(''Times: '',53f12.3)') tbnd(1,1),tval(1),tbnd(2,1),tval(1)-tbnd(1,1),tbnd(2,1)-tval(1)
                     error_flag = cmor_write(          &
                          var_id        = cmor_var_id, &
                          data          = indat2a,     &
