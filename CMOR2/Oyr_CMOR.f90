055e964ba8d9ea5fcb4145fed40cfb203ea9d526
Gary Strand <strandwg@users.noreply.github.com>
2012-06-25 20:00:48 +0000

    Oyr_CMOR changes

--- CMOR2/Oyr_CMOR.f90
+++ CMOR2/Oyr_CMOR.f90
@@ -318,7 +318,7 @@ program Oyr_CMOR
                 ntimes_passed = 1,           &
                 time_vals     = tval,        &
                 time_bnds     = tbnd)
-           write(*,'(''cmor_write id '',i5,'' # '',i5,'' flag '',i5,'' T '',i5,'' t '',e12.4)') cmor_var_id(ixw),ixw,error_flag,tcount,tval(1)
+           write(*,'(''cmor_write id '',i5,'' # '',i5,'' flag '',i5,'' T '',i5,'' t '',f12.3)') cmor_var_id(ixw),ixw,error_flag,tcount,tval(1)
            if (error_flag < 0) then
               write(*,'(''ERROR writing '',a,'' T# '',i6)') trim(xw(ixw)%entry),it
               stop
