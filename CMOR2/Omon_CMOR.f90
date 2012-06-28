914c24576a4af82d1e909938ed8b5df415529776
Gary Strand <strandwg@users.noreply.github.com>
2012-06-28 17:00:33 +0000

    More changes to Omon_CMOR

--- CMOR2/Omon_CMOR.f90
+++ CMOR2/Omon_CMOR.f90
@@ -3680,12 +3680,13 @@ program Omon_CMOR
                     !
                     cmordat2d = merge(0.,spval,kmt.gt.0)
                     call read_var(myncid(ifile,1),var_info(var_found(ifile,1))%name,indat3a)
-                    do k = 11,nlevs
+                    do k = 1,nlevs
                        do j = 1,nlats
                           do i = 1,nlons
                              if (indat3a(i,j,k).ne.spval) then
                                 if (kmt(i,j).le.10) then
-                                   cmordat2d(i,j) = spval
+!                                   cmordat2d(i,j) = spval
+                                   cmordat2d(i,j) = 0.
                                 else
                                    cmordat2d(i,j) = cmordat2d(i,j)+(indat3a(i,j,k))
                                 endif
